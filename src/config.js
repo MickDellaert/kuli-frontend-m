7f3bb64865c9174fc59a0f465dc77dd107fde42b
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-09-18 18:42:48 +0200

    local url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const ORIGIN = "https://kuli-strapi.herokuapp.com/api/";
+// export const ORIGIN = "https://kuli-strapi.herokuapp.com/api/";
+export const ORIGIN = "http://localhost:1337/api/";
+
